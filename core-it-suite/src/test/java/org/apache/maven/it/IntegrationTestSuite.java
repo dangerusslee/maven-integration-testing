2b4f1965a5ed2f57321e2eaf83d95a3d848c2531
Christian Schulte <schulte@apache.org>
2016-01-30 20:21:33 +0100

    [MNG-4463] Version ranges cannot be used for artifacts with 'import' scope

--- core-it-suite/src/test/java/org/apache/maven/it/IntegrationTestSuite.java
+++ core-it-suite/src/test/java/org/apache/maven/it/IntegrationTestSuite.java
@@ -106,6 +106,7 @@ public class IntegrationTestSuite
         // -------------------------------------------------------------------------------------------------------------
         // suite.addTestSuite( MavenIT0108SnapshotUpdateTest.class ); -- MNG-3137
 
+        suite.addTestSuite( MavenITmng4463DependencyManagementImportVersionRanges.class );
         suite.addTestSuite( MavenITmng5958LifecyclePhaseBinaryCompat.class );
         suite.addTestSuite( MavenITmng5898BuildMultimoduleWithEARFailsToResolveWARTest.class );
         suite.addTestSuite( MavenITmng5840ParentVersionRanges.class );
